8288de6356278fe1a1c0feefb7172cd220ce1cd6
FITONE GEORGES <fitonegeorges@gmail.com>
2024-04-11 17:07:17 +0100

    Adding files to the repository

---
+++ resources/views/registration.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+@extends('header')
+@section('title','registration')
+@section('content')
+    <div class="container">
+
+        <form>
+            <div class="mb-3">
+                <label for="Name" class="form-label">Name</label>
+                <input type="name" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="Phone" class="form-label">Phone</label>
+                <input type="phone" class="form-control" id="phone">
+            </div>
+            <div class="mb-3">
+                <label for="Email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">example henryjohn@gmail.com.</div>
+            </div>
+            <div class="mb-3">
+                <label for="Password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Click</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
+@endsection
